ad64f6df8e500e78007f6d6337294887c07b94e6
Kenny Mafabatho <mafabatholehlohonolo@gmail.com>
2024-04-23 11:33:34 +0200

    Added maven jacskon databinding dependecy and added values to pojos

--- src/test/java/com/testautomation/apitesting/tests/PostAPIRequestUsingPojos.java
+++ src/test/java/com/testautomation/apitesting/tests/PostAPIRequestUsingPojos.java
@@ -1,4 +1,27 @@
 package com.testautomation.apitesting.tests;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.testautomation.apitesting.pojos.Booking;
+import com.testautomation.apitesting.pojos.BookingDates;
+import org.testng.annotations.Test;
+
 public class PostAPIRequestUsingPojos {
+
+    @Test
+    public void PostAPIRequest(){
+
+        try {
+            BookingDates bookingDates = new BookingDates("2024-04-20", "2024-04-25");
+            Booking booking = new Booking("api test", "Practise","breakfast",2000,true, bookingDates);
+
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            String requestBody = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(booking);
+
+            System.out.println(requestBody);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
 }
